da067d5e1ade0098db2c679bbbfda815875d4c85
chuyxxy <chuyxxy@gmail.com>
2022-12-05 01:19:07 -0600

    Player Controller

---
+++ ProyectoFinal2_Matematicas/Assets/CharacterController.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class CharacterController : MonoBehaviour
+{
+
+ 
+        [Header("MOVEMENT")]
+
+       public float MoveSpeed;
+    private Vector2 CurMovementInput;
+    public float JumpForce;
+    public LayerMask GroundLayerMask;
+
+    [Header("LOOK")]
+
+    public Transform CameraContainer;
+    public float minXLook;
+    public float MaxXLook;
+    private float CAmCurXrot;
+    public float LookSensitivity;
+    private Vector2 MouseDelta;
+
+
+    //componets
+    private Rigidbody Rig;
+    private void Awake()
+    {
+        Rig = GetComponent<Rigidbody>();
+    }
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void LateUpdate()
+    {
+        CameraLook();
+    }
+    void Move()
+    {
+        Vector3 dir = transform.forward * CurMovementInput.y + transform.right * CurMovementInput.x;
+        dir *= MoveSpeed;
+        dir.y = Rig.velocity.y;
+        Rig.velocity = dir;
+    }
+     void CameraLook()
+    {
+        CAmCurXrot += MouseDelta.y * LookSensitivity;
+        CAmCurXrot = Mathf.Clamp(CAmCurXrot, minXLook, MaxXLook);
+        CameraContainer.localEulerAngles = new Vector3(-CAmCurXrot, 0, 0);
+        transform.eulerAngles += new Vector3(0, MouseDelta.x * LookSensitivity, 0);
+    }
+   public void OnlookInput(InputAction.CallbackContext context)
+    {
+        MouseDelta = context.ReadValue<Vector2>();
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            CurMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            CurMovementInput = Vector2.zero;
+        }
+    }
+    public void OnJumpIput(InputAction.CallbackContext context)
+    {
+        Rig.AddForce(new Vector3(0, JumpForce), ForceMode.Impulse);
+    }
+  /*  bool IsGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray (transform.position+(transform.forward*.2f)+(Vector3.up*.01f),Vector3.down ),
+            new Ray (transform.position+(-transform.forward*.2f)+(Vector3.up*.01f),Vector3.down ),
+            new Ray (transform.position+(transform.right*.2f)+(Vector3.up*.01f),Vector3.down ),
+            new Ray (transform.position+(-transform.right*.2f)+(Vector3.up*.01f),Vector3.down ),
+        };
+        for (int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, GroundLayerMask))
+            {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position + (transform.forward * .2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (-transform.forward * .2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (transform.right * .2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (-transform.right * .2f), Vector3.down);
+    }*/
+}
